948db30c52926f8b042cf5f1fec0354f1123db07
youjeongd <114713451+youjeongd@users.noreply.github.com>
2024-05-03 20:53:50 +0900

    chase&stairs

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
@@ -8,26 +9,56 @@ using UnityEngine.UI;
 //요원(agent=enemy)에게 목적지를 알려줘서 목적지로 이동.
 public class Enemy : MonoBehaviour
 {
-    //목적지
+    Rigidbody rigid;
+    BoxCollider boxCollider;
+    Material mat;
+    NavMeshAgent nav;
+    Animator anim;
+     
     public Transform target;
-    //요원
-    NavMeshAgent agent;
+    public bool isChase;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        //요원을 정의해줘서
-        agent = GetComponent<NavMeshAgent>();
-        //생성될때 목적지(Player)를 찿는다.
-        target = GameObject.Find("Player").transform;
-        //요원에게 목적지를 알려준다.
-        agent.destination = target.transform.position;
+        rigid = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        mat = GetComponentInChildren<MeshRenderer>().material;
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        Invoke("ChaseStart", 2);//chasestart 2초 후에
     }
 
-    // Update is called once per frame
+    void FreezeVelocity()
+    {
+        if(isChase)
+        {
+            rigid.angularVelocity = Vector3.zero;//enemy와 player 충돌시 속도 변화 방지
+            rigid.velocity = Vector3.zero;
+        }
+        
+    }
+
+    void ChaseStart()
+    {
+        isChase = true;
+
+        anim.SetBool("IsWalk", true);
+    }
     void Update()
     {
+        if (isChase)
+        {
+            nav.SetDestination(target.position);//추적 게시
+        }
+        
 
     }
 
+    void FixedUpdate()
+    {
+        FreezeVelocity();
+    }
 }
+
+
