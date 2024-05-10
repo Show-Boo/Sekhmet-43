a00b7a6f686e05a11f1eede466c2da2f51dd1b2b
youjeongd <114713451+youjeongd@users.noreply.github.com>
2024-05-10 23:09:57 +0900

    attack

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -17,6 +17,8 @@ public class Enemy : MonoBehaviour
      
     public Transform target;
     public bool isChase;
+    public bool isAttack;
+    public BoxCollider meleeArea;
 
     void Awake()
     {
@@ -47,16 +49,52 @@ public class Enemy : MonoBehaviour
     }
     void Update()
     {
-        if (isChase)
+        if (nav.enabled)//이게 뭔소린지 모르겠음..
         {
             nav.SetDestination(target.position);//추적 게시
+            nav.isStopped = !isChase;
         }
-        
 
     }
 
+    void Targerting()
+    {
+        float targetRadius = 1.5f;
+        float targetRange = 3f;
+        RaycastHit[] rayHits = Physics.SphereCastAll(transform.position, targetRadius, transform.forward, targetRange, LayerMask.GetMask("Player"));
+        if (rayHits.Length > 0 && !isAttack)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        //정지
+        isChase =false;
+        isAttack = true;
+        //animation 호출
+        anim.SetBool("IsAttack",true);
+        //delay 주기
+        yield return new WaitForSeconds(0.2f);
+
+        //공격범위활성화
+        meleeArea.enabled = true;
+
+        //delay 주기
+        yield return new WaitForSeconds(1f);
+        meleeArea.enabled = false;
+
+        //정지
+        isChase = true;
+        isAttack = false;
+        //animation 호출
+        anim.SetBool("IsAttack", false);
+
+    }
     void FixedUpdate()
     {
+        Targerting();
         FreezeVelocity();
     }
 }
