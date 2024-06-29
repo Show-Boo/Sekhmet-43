3aef7e7a9dec76290f5191bd248d684dfa0add90
youjeongd <114713451+youjeongd@users.noreply.github.com>
2024-06-29 18:32:26 +0900

    Hiding..

--- Assets/Scripts/PlayerHiding.cs
+++ Assets/Scripts/PlayerHiding.cs
@@ -1,26 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHiding : MonoBehaviour
 {
-
     public Transform Player1; // Player1의 Transform
     public Transform HidePlayer; // Player2의 Transform
     private Camera PlayerCamera; // Player1의 카메라
     private Camera HidePlayerCamera; // Player2의 카메라
     public float switchDistance = 5.0f; // 거리 임계값
-
     public bool isPlayer1Active = true; // 현재 활성화된 플레이어 여부
 
     void Start()
     {
         // 시작 시 Player1 카메라 활성화, Player2 카메라 비활성화
-        PlayerCamera.gameObject.SetActive(true);
-        HidePlayerCamera.gameObject.SetActive(false);
-
         PlayerCamera = Player1.GetComponentInChildren<Camera>();
         HidePlayerCamera = HidePlayer.GetComponentInChildren<Camera>();
+
+        if (PlayerCamera != null)
+            PlayerCamera.gameObject.SetActive(true);
+        else
+            Debug.LogError("Player1 does not have a Camera component!");
+
+
+        if (HidePlayerCamera != null)
+            HidePlayerCamera.gameObject.SetActive(false);
+        else
+            Debug.LogError("HidePlayer does not have a Camera component!");
     }
 
     void Update()
@@ -36,11 +43,18 @@ public class PlayerHiding : MonoBehaviour
 
     void SwitchCamera()
     {
+        // null 체크
+        if (PlayerCamera == null || HidePlayerCamera == null)
+        {
+            Debug.LogError("Camera component is null! Make sure cameras are assigned.");
+            return;
+        }
+
         if (isPlayer1Active)
         {
             // Player2 카메라 활성화, Player1 카메라 비활성화
+            HidePlayerCamera.gameObject.SetActive(true);//활성화부터 시켜주기
             PlayerCamera.gameObject.SetActive(false);
-            HidePlayerCamera.gameObject.SetActive(true);
         }
         else
         {
@@ -51,7 +65,8 @@ public class PlayerHiding : MonoBehaviour
 
         // 활성화된 플레이어 상태 업데이트
         isPlayer1Active = !isPlayer1Active;
+        Debug.Log("isPlayer1Active: " + isPlayer1Active);
     }
-
 }
+
 
