e5675e0290d1957a0c726a418aa49a6183cad7a7
Leejaewonnn <leejaewon217@daum.net>
2024-07-01 20:51:01 +0900

    cardkeydoor

--- Assets/Scripts/CardkeyDoor.cs
+++ Assets/Scripts/CardkeyDoor.cs
@@ -1,53 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
+public class DoorRaycast : MonoBehaviour
 {
-    public bool hasKeyCard = false; // 카드키 보유 여부를 추적하는 변수
+    [SerializeField] private int rayLength = 5;
+    [SerializeField] private LayerMask layerMaskInteract;
+    [SerializeField] private string excludeLayerName = null;
 
-    // 열려있는 문을 나타내는 변수
-    public GameObject door;
-    private bool doorOpen = false;
+    private MyDoorController raycastedObj;
 
-    void Update()
+    [SerializeField] private KeyCode openDoorKey = KeyCode.E;
+
+    [SerializeField] private Image crosshair = null;
+    private bool isCrosshairActive;
+    private bool doOnce;
+
+    private const string interactableTag = "InteractiveObject";
+
+    private void Update()
     {
-        // "E" 키를 누르면 문을 열거나 닫기
-        if (Input.GetKeyDown(KeyCode.E))
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        int mask = 1 << LayerMask.NameToLayer(excludeLayerName) | layerMaskInteract.value;
+
+        if (Physics.Raycast(transform.position, fwd, out hit, rayLength, mask))
         {
-            if (doorOpen)
+            if (hit.collider.CompareTag(interactableTag))
             {
-                CloseDoor();
+                if (!doOnce)
+                {
+                    raycastedObj = hit.collider.gameObject.GetComponent<MyDoorController>();
+                    CrosshairChange(true);
+                }
+
+                isCrosshairActive = true;
+                doOnce = true;
+
+                if (Input.GetKeyDown(openDoorKey))
+                {
+                    raycastedObj.PlayAnimation();
+                }
             }
-            else
+        }
+        else
+        {
+            if (isCrosshairActive)
             {
-                // 문이 닫혀있고 카드키를 가지고 있다면 문을 열기
-                if (hasKeyCard)
-                {
-                    OpenDoor();
-                }
-                else
-                {
-                    Debug.Log("카드키가 필요합니다!");
-                }
+                CrosshairChange(false);
+                doOnce = false;
             }
         }
     }
 
-    // 문을 열기
-    void OpenDoor()
+    void CrosshairChange(bool on)
     {
-        door.SetActive(false); // 문을 열어야 하지만 여기서는 비활성화로 대체
-        doorOpen = true;
-        Debug.Log("문이 열렸습니다!");
-    }
-
-    // 문을 닫기
-    void CloseDoor()
-    {
-        door.SetActive(true); // 문을 닫아야 하지만 여기서는 활성화로 대체
-        doorOpen = false;
-        Debug.Log("문이 닫혔습니다.");
+        if (on && !doOnce)
+        {
+            crosshair.color = Color.red;
+        }
+        else
+        {
+            crosshair.color = Color.white;
+            isCrosshairActive = false;
+        }
     }
 }
 
