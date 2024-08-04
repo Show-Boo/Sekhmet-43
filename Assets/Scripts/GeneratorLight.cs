7399c06f048f33628a13651ab85c3577bd388f5e
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2024-08-04 13:06:31 +0900

    light
    preprocessing+generator light

---
+++ Assets/Scripts/GeneratorLight.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+public class GeneratorLight : MonoBehaviour
+{
+
+    public Light _Light;
+
+    public float MinTime;
+    public float MaxTime;
+    public float Timer;
+
+    public AudioSource AS;
+    public AudioClip LightAudio;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer = Random.Range(MinTime, MaxTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlickerLight();
+    }
+
+    void FlickerLight()
+    {
+        if (Timer > 0)
+            Timer -= Time.deltaTime;
+        if (Timer <= 0)
+        {
+            _Light.enabled = !_Light.enabled;
+            Timer = Random.Range(MinTime, MaxTime);
+            AS.PlayOneShot(LightAudio);
+        }
+    }
+
+}
+
