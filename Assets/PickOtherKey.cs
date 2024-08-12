91108d5a220c60db0aea09ed1610fbe44d26dbdc
Leejaewonnn <leejaewon217@daum.net>
2024-08-12 15:44:43 +0900

    keycard

---
+++ Assets/PickOtherKey.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickOtherKey : MonoBehaviour
+{
+    public Component doorcolliderhere;
+    public GameObject keygone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay()
+    {
+
+        if (Input.GetKey(KeyCode.E))
+            doorcolliderhere.GetComponent<BoxCollider>().enabled = true;
+
+        if (Input.GetKey(KeyCode.E))
+            keygone.SetActive(false);
+
+
+    }
+}
