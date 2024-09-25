d240faa90a8e30fd61e0f85774676b98cca3fe62
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2024-09-26 03:59:51 +0900

    cutscene
    modify

--- Assets/Scripts/Test_2/CutSceneController.cs
+++ Assets/Scripts/Test_2/CutSceneController.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
-
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.SceneManagement;
 
 public class CutSceneController : MonoBehaviour
 {
     public VideoPlayer[] videoPlayers; // VideoPlayer 컴포넌트를 연결합니다.
     public PlayerMovement playerController; // PlayerMovement와 같은 플레이어 컨트롤러 스크립트를 연결합니다.
-
+    public PostProcessVolume postProcessVolume;
 
     public int nowIndex = 0;
     public int previousIndex = 0;
@@ -29,6 +29,8 @@ public class CutSceneController : MonoBehaviour
     void DisablePlayerControl(VideoPlayer vp)
     {
         //playerController.enabled = false;//컷씬 시작하면 player 멈춰주기
+        postProcessVolume.enabled = false; // Volume 자체를 비활성화
+        Debug.Log("PostProcess Volume Disabled");
     }
 
     void EnablePlayerControl(VideoPlayer vp)
@@ -42,8 +44,12 @@ public class CutSceneController : MonoBehaviour
         if (Scenechange)
         {
             ChangeSceneByName("myproject");
-        }
 
+            
+        }
+        // 컷씬이 끝났을 때 PostProcessing Volume 다시 활성화
+        postProcessVolume.enabled = true; // Volume 자체를 다시 활성화
+        Debug.Log("PostProcess Volume Enabled");
 
     }
 
