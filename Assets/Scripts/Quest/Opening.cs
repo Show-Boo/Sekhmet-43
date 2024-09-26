558ea18f4fdeb5b283993ae8a68123bae0f3ab20
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2024-09-26 17:45:14 +0900

    cutscene2
    sdf

--- Assets/Scripts/Quest/Opening.cs
+++ Assets/Scripts/Quest/Opening.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.Rendering.PostProcessing;
 
 public class Opening : MonoBehaviour
 {
 
     public VideoPlayer OpeningVideo; // VideoPlayer 컴포넌트를 연결합니다.
     public PlayerMovement playerController;
+    public PostProcessVolume postProcessVolume;
 
     public BoxCollider[] boxColliders;
 
@@ -32,12 +34,17 @@ public class Opening : MonoBehaviour
     void DisablePlayerControl(VideoPlayer vp)
     {
         playerController.enabled = false;//컷씬 시작하면 player 멈춰주기
+        postProcessVolume.enabled = false; // Volume 자체를 비활성화
+        Debug.Log("PostProcess Volume Disabled");
     }
 
     void EnablePlayerControl(VideoPlayer vp)
     {
         playerController.enabled = true;//컷씬 끝나면 player 이동 가능하게 해주기
         OpeningVideo.enabled = false;
+        // 컷씬이 끝났을 때 PostProcessing Volume 다시 활성화
+        postProcessVolume.enabled = true; // Volume 자체를 다시 활성화
+        Debug.Log("PostProcess Volume Enabled");
     }
 }
 
