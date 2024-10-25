8f2d9dafdab24be911241a8994bec657cb36b7d8
youjeongd <114713451+youjeongd@users.noreply.github.com>
2024-10-25 17:41:02 +0900

    aa

--- Assets/Scripts/YouDied_Manager.cs
+++ Assets/Scripts/YouDied_Manager.cs
@@ -1,12 +1,17 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class YouDied_Manager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Button TryAgain;
+    
     void Start()
     {
+        TryAgain.enabled = false;
         
     }
 
