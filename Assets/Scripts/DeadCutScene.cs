11b53f8216a63d589910a285c1038db9759c88b8
youjeongd <114713451+youjeongd@users.noreply.github.com>
2024-10-25 17:39:07 +0900

    ing

--- Assets/Scripts/DeadCutScene.cs
+++ Assets/Scripts/DeadCutScene.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class DeadCutScene : MonoBehaviour
 {
     // Start is called before the first frame update
     public CheckPoint checkPoint;
+
+    //컷씬 끝나면 검은 화면 활성화+죽는 컷씬 비활성화 -> text 깜빡임 구현 -> 엔터키or버튼누르기 -> 검은화면 사라짐 + 화면 비활성화.
+
+    public GameObject blackOut;
     void Start()
     {
         VideoPlayer videoPlayer = GetComponent<VideoPlayer>();//자체 오브젝트에서 가져오기
@@ -27,7 +32,8 @@ public class DeadCutScene : MonoBehaviour
         checkPoint.restart = true;//컷씬 끝나고 돌아가기
         Debug.Log("checkpoint");//디버그 되는데..
 
-        
+        blackOut.SetActive(true);
+
         gameObject.SetActive(false);//자체 비활성화
 
 
