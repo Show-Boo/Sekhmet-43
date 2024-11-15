7c98dddfd52ec683eae4e1eff9b4e0fdada4842d
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2024-11-15 22:11:40 +0900

    disabled stamina during deadcutscene

--- Assets/Scripts/DeadCutScene.cs
+++ Assets/Scripts/DeadCutScene.cs
@@ -7,39 +7,47 @@ using UnityEngine.UI;
 
 public class DeadCutScene : MonoBehaviour
 {
-    // Start is called before the first frame update
     public CheckPoint checkPoint;
     public PlayerMovement playerController;
-    //컷씬 끝나면 검은 화면 활성화+죽는 컷씬 비활성화 -> text 깜빡임 구현 -> 엔터키or버튼누르기 -> 검은화면 사라짐 + 화면 비활성화.
-
+    public StaminaController staminaController;
     public GameObject blackOut;
+
     void Start()
     {
-        //blackOut.SetActive(false);이 스크립트는 죽고 나서 활성화 되므로 이 코드는 무의미함
-        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();//자체 오브젝트에서 가져오기
+        // VideoPlayer 컴포넌트 가져오기
+        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
 
+        // 비디오 시작과 끝에 대한 함수 등록
         videoPlayer.started += StartControl;
-        videoPlayer.loopPointReached += EndControl;//함수등록
-
+        videoPlayer.loopPointReached += EndControl;
     }
 
-    // Update is called once per frame
     void StartControl(VideoPlayer vp)
     {
+        // 스태미나 UI 비활성화
+        staminaController.DisableForCutscene();
+
+        // 플레이어 컨트롤 비활성화
         playerController.enabled = false;
     }
 
     void EndControl(VideoPlayer vp)
     {
-        checkPoint.restart = true;//컷씬 끝나고 돌아가기
-        Debug.Log("checkpoint");//디버그 되는데..
+        // 스태미나 UI 활성화
+        staminaController.EnableAfterCutscene();
 
-        blackOut.SetActive(true);//검은화면
+        // 플레이어 컨트롤 다시 활성화
+        playerController.enabled = true;
 
-        gameObject.SetActive(false);//자체 비활성화
+        // 체크포인트 리스타트
+        checkPoint.restart = true;
 
+        Debug.Log("checkpoint");
 
+        // 검은 화면 활성화
+        blackOut.SetActive(true);
+
+        // 컷씬 오브젝트 비활성화
+        gameObject.SetActive(false);
     }
-
-    
 }
