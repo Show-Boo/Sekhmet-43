02360a129a23d958e7cf0aee72607e9dd57b0565
youjeongd <114713451+youjeongd@users.noreply.github.com>
2024-11-23 20:21:20 +0900

    aa
    격납고/퀘스트연결/괴물 끼이는 거 고침

--- Assets/Scripts/Quest/Q_4.cs
+++ Assets/Scripts/Quest/Q_4.cs
@@ -9,7 +9,7 @@ public class Q_4 : Q_ParentClass
     // Start is called before the first frame update
     public override bool cutscene { get => false_cutscene; set => false_cutscene = value; }
     
-    public DisplayUIOnApproach displayUIOnApproach; //{ get; set; }-> 이걸 꼭 써야하나? ui system스크립트
+    public DoorOpenUI displayUIOnApproach; //{ get; set; }-> 이걸 꼭 써야하나? ui system스크립트
     //public QuestManager QuestManager;//오류생겨서 get set 버림..
     public GameObject questUI; // 퀘스트 UI를 나타내는 GameObject 추가
 
